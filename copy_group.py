e2a6836b7b7d810a55fde69a21dc63cb561eea2a
Bacha04 <114564813+Bacha04@users.noreply.github.com>
2024-11-26 20:04:26 +0200

    Delete copy_group.py

--- copy_group.py
+++
@@ -1,8 +0,0 @@
-class Group:
-    def __init__(self, group_name, people_num, subjects_hours):
-        self.group_name = group_name
-        self.people_num = people_num
-        self.subjects_hours = subjects_hours
-
-    def __str__(self):
-        return f"Group(group_name={self.group_name}, people_num={self.people_num}, subjects_hours={self.subjects_hours})"
